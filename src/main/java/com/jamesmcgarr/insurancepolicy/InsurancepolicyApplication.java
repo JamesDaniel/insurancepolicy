a10456669e96136d3f8b776028fe33f2ebcde321
jmcgarr <jmcgarr@fexco.com>
2022-05-25 23:30:41 +0100

    initial commit

---
+++ src/main/java/com/jamesmcgarr/insurancepolicy/InsurancepolicyApplication.java
@@ -0,0 +1,13 @@
+package com.jamesmcgarr.insurancepolicy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurancepolicyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsurancepolicyApplication.class, args);
+	}
+
+}
